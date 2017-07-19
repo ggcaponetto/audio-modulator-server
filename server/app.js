9ad4babca95206d30c725ea9f985f5e3e744a021
Giuseppe Caponetto <giuseppe.caponetto@foodiestrip.com>
2017-07-19 16:57:06 +0200

    add working ws

--- server/app.js
+++ server/app.js
@@ -2,11 +2,11 @@ const express = require('express')
 var path = require('path');
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+const APP_PORT = 3000;
 
 const buildFolderPath = 'build';
 app.use(express.static(buildFolderPath));
 
-app.listen(PORT, function () {
-  console.log('AudioModulator app is listening on port ' +  PORT);
+app.listen(APP_PORT, function () {
+  console.log('AudioModulator app is listening on port ' +  APP_PORT);
 })
--- server/socketApp.js
+++ server/socketApp.js
@@ -2,11 +2,11 @@ var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
-const PORT = 3001;
-server.listen(PORT);
+const WS_PORT = 8080;
+server.listen(WS_PORT);
 
 io.on('connection', function (socket) {
-  console.log('Socket connection open on port: ' + PORT);
+  console.log('Socket connection open on port: ' + WS_PORT);
   socket.emit('news', { hello: 'world' });
   socket.on('my other event', function (data) {
     console.log(data);
