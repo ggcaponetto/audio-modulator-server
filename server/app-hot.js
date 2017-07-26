4b9366a9affaa66a0351f2150c5f22b14f922e44
Giuseppe Caponetto <giuseppe.caponetto@foodiestrip.com>
2017-07-26 09:05:35 +0200

    wip

--- server/app-hot.js
+++ server/app-hot.js
@@ -6,7 +6,6 @@ const express = require('express');
 
 const app = express();
 const fs = require('fs');
-const config = require('../config/config.js').config;
 
 const port = 5000;
 
@@ -15,41 +14,24 @@ require.extensions['.html'] = function (module, filename) {
   module.exports = fs.readFileSync(filename, 'utf8');
 };
 
-const index = require('../build/index.html');
-// inject the heroku port inside config
-const NODE_ENV = process.env.NODE_ENV;
-console.log(`Running server/app.js with NODE_ENV=${NODE_ENV}.`);
-config.development.port = port;
-config.production.port = port;
-config.development.env = NODE_ENV;
-config.production.env = NODE_ENV;
-
-app.use(express.static(`${__dirname}/../build-custom-no-index`));
-app.use('/', (req, res) => {
-  const modified = index.replace('__AM_DATA__', JSON.stringify(config[`${NODE_ENV}`], null, 4));
-  res.set('Content-Type', 'text/html');
-  res.send(new Buffer(modified));
-  console.log('Sent index.html (from build folder) with config injection.', JSON.stringify(config[NODE_ENV], null, 4));
-});
-
 const server = http.createServer(app);
 server.listen(port);
 
 console.log('Http server listening on port %d .', port);
 
 const wss = new WebSocketServer({ server });
-console.log('Websocket server created.');
+console.log('Websocket server (dev/hot) created.');
 
 wss.on('connection', (ws) => {
   const id = setInterval(() => {
     const message = { midiTest: true };
-    console.log('Sending msg..');
+    console.log('Sending msg (dev/hot)..');
     ws.send(JSON.stringify(message), () => {});
   }, 1000);
 
-  console.log('Websocket connection opened.');
+  console.log('Websocket connection (dev/hot) opened.');
   ws.on('close', () => {
-    console.log('Websocket connection closed.');
+    console.log('Websocket connection (dev/hot) closed.');
     clearInterval(id);
   });
 });
