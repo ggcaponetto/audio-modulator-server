e111a20035a70065a89bb8f4d8ca8bdc1f45f3d7
Giuseppe Caponetto <giuseppe.caponetto@foodiestrip.com>
2017-07-29 10:22:37 +0200

    wip

--- server/websocket.js
+++ server/websocket.js
@@ -3,17 +3,21 @@ const Connector = require('./connector.js').Connector;
 const run = (name, wss, connector) => {
   wss.on('connection', (ws) => {
     console.log(`${name}: connection opened.`);
+
+    // Send a unique request id
     const browserRequestId = connector.addBrowserRequest();
     console.log('connector status: \n' + JSON.stringify(connector));
     const pairingMessage = { type: 'browserRequestId', payload: { browserRequestId } };
     console.log(`${name}: sending a browser request id.`);
     ws.send(JSON.stringify(pairingMessage), () => {});
+
     // Send heartbeat
     const id = setInterval(() => {
       const message = { type: 'heartBeat', payload: { timestampServerEmit: Date.now() } };
       console.log(`${name}: sending a heart beat`);
       ws.send(JSON.stringify(message), () => {});
     }, 1000);
+
     // Clear heartbeat interval
     ws.on('close', () => {
       console.log(`${name}: connection closed.`);
